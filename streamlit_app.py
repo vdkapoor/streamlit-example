4faa1c86c34723185d42567cb9906de774c46be2
Varun Kapoor <sin.gage@gmail.com>
2023-02-11 18:47:55 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,56 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import yfinance as yf
+import datetime
+import plotly.express as px
+import plotly.graph_objs as go
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+import requests
+import pandas as pd
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def get_falling_stocks(asx_stocks):
+    falling_stocks = []
+    for stock in asx_stocks:
+        stock_data = yf.Ticker(stock + ".AX").history(period="5d")
+        if (stock_data["Close"][1] < stock_data["Close"][0]):
+          if (stock_data["Close"][2] < stock_data["Close"][1]):
+            if (stock_data["Close"][3] < stock_data["Close"][2]):
+              if (stock_data["Close"][4] < stock_data["Close"][3]):    
+                st.write('yes')
+                falling_stocks.append(stock)
+    return falling_stocks
+
+asx_20_stocks = ['AGVT', 'LNAS']
+falling_stocks = get_falling_stocks(asx_20_stocks)
+st.write(falling_stocks)
+
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+import yfinance as yf
+
+
+
+def chart(stock):
+    stock_data = yf.Ticker(stock+".AX").history(period="2y",interval="1wk")
+    st.header(stock)
+  
+    fig = go.Figure(data=[go.Candlestick(x=stock_data.index,
+                    open=stock_data['Open'],
+                    high=stock_data['High'],
+                    low=stock_data['Low'],
+                    close=stock_data['Close'])])
+    st.plotly_chart(fig)
+    return 
+asx_20_stocks = ['LNAS', 'WAM','NST','MFF','CPU']
+for stock in asx_20_stocks:
+  chart(stock)
