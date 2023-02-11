07b95ad3fd6c46b41fbb1f74c673251d992a0be9
Varun Kapoor <sin.gage@gmail.com>
2023-02-12 00:24:49 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ st.write(falling_stocks)
 def chart(stock):
     stock_data = yf.Ticker(stock+".AX").history(period="2y",interval="1wk")
     st.header(stock)
-    st.write(yf.Ticker(stock))
+    st.write(yf.Ticker(stock).info)
   
     fig = go.Figure(data=[go.Candlestick(x=stock_data.index,
                     open=stock_data['Open'],
